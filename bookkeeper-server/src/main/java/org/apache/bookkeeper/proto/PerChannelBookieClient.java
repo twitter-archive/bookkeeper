fe73358d505390ff791762763c4db4d4db122cc3
Aniruddha Laud <aniruddha@twitter.com>
2012-10-13 15:54:20 -0700

    Fix NPE in PCBC  - Fix NPE in PCBC
    RB_ID=91303

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PerChannelBookieClient.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PerChannelBookieClient.java
@@ -357,8 +357,12 @@ public class PerChannelBookieClient extends SimpleChannelHandler implements Chan
                 ReadCompletion readCompletion = readCompletions.remove(key);
 
                 if (readCompletion != null) {
+                    String bAddress = "null";
+                    if (null != channel) {
+                        bAddress = channel.getRemoteAddress().toString();
+                    }
                     LOG.error("Could not write  request for reading entry: " + key.entryId + " ledger-id: "
-                              + key.ledgerId + " bookie: " + channel.getRemoteAddress());
+                              + key.ledgerId + " bookie: " + bAddress);
 
                     readCompletion.cb.readEntryComplete(BKException.Code.BookieHandleNotAvailableException,
                                                         key.ledgerId, key.entryId, null, readCompletion.ctx);
