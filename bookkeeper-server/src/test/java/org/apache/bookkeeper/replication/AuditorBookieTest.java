fcc78c33513814e3cd2a33a7b9d38348120ec052
Ivan Brendan Kelly <ivank@apache.org>
2013-01-07 21:53:54 +0000

    BOOKKEEPER-534: Flakeyness in AuditorBookieTest (umamahesh via ivank)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/bookkeeper/trunk@1430041 13f79535-47bb-0310-9956-ffa450edef68

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/replication/AuditorBookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/replication/AuditorBookieTest.java
@@ -289,12 +289,11 @@ public class AuditorBookieTest extends BookKeeperClusterTestCase {
             List<BookieServer> auditors = getAuditorBookie();
             if (auditors.size() > 0) {
                 newAuditor = auditors.get(0);
-                if (auditor == newAuditor) {
-                    Thread.sleep(500);
-                } else {
+                if (auditor != newAuditor) {
                     break;
                 }
             }
+            Thread.sleep(500);
             retryCount--;
         }
         Assert.assertNotNull(
